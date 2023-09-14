fbf03bffd24391cb961892c307fd7a43d4503056
LeoDm123 <leo.meiners1@gmail.com>
2023-09-14 10:41:46 -0300

    delete otro back

--- src/routes/menu.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { AddMenu } = require("../controllers/menu.controllers");
-
-const routerMenu = express.Router();
-
-routerMenu.post("/AddMenu", AddMenu);
-
-module.exports = routerOp;
